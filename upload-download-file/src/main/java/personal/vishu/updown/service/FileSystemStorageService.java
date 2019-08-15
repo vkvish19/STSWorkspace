5e394d270667cb8c06da8574ac399885bea95233
vkvish19 <vkvish19@gmail.com>
2019-08-15 20:15:52 +0530

    spring boot application for uploading and downloading files from particular server where the application will be run.

---
+++ upload-download-file/src/main/java/personal/vishu/updown/service/FileSystemStorageService.java
@@ -0,0 +1,121 @@
+package personal.vishu.updown.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import personal.vishu.updown.exception.StorageException;
+import personal.vishu.updown.exception.StorageFileNotFoundException;
+import personal.vishu.updown.properties.StorageProperties;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
+
+@Service
+public class FileSystemStorageService implements StorageService
+{
+    private final Path rootLocation;
+
+    @Autowired
+    public FileSystemStorageService(StorageProperties properties)
+    {
+        this.rootLocation = Paths.get(properties.getLocation());
+    }
+
+    @Override
+    public void init() throws StorageException {
+        try
+        {
+            Files.createDirectories(rootLocation);
+        }
+        catch(IOException ioe)
+        {
+            throw new StorageException("Could not initialize storage", ioe);
+        }
+    }
+
+    @Override
+    public void store(MultipartFile file)
+    {
+        String filename = StringUtils.cleanPath(file.getOriginalFilename());
+        try
+        {
+            if(file.isEmpty())
+            {
+                throw new StorageException(String.format("Failed to store empty file %s", filename));
+            }
+            if(filename.contains(".."))
+            {
+                // security check
+                throw new StorageException(String.format("Cannot store file with relative path " +
+                        "outside current directory ", filename));
+            }
+            try(InputStream inputStream = file.getInputStream())
+            {
+                Files.copy(inputStream, this.rootLocation.resolve(filename), StandardCopyOption.REPLACE_EXISTING);
+            }
+        }
+        catch(IOException ioe)
+        {
+            throw new StorageException(String.format("Failed to store file %s", filename), ioe);
+        }
+    }
+
+    @Override
+    public Stream<Path> loadAll()
+    {
+        try
+        {
+            return Files.walk(this.rootLocation, 1)
+                    .filter(path -> !path.equals(this.rootLocation))
+                    .map(this.rootLocation::relativize);
+        }
+        catch (IOException ioe)
+        {
+            throw new StorageException("Failed to read stored File", ioe);
+        }
+    }
+
+    @Override
+    public Path load(String filename)
+    {
+        return rootLocation.resolve(filename);
+    }
+
+    @Override
+    public Resource loadAsResource(String filename)
+    {
+        try
+        {
+            Path file = load(filename);
+            Resource resource = new UrlResource(file.toUri());
+            if(resource.exists() || resource.isReadable())
+            {
+                return resource;
+            }
+            else
+            {
+                throw new StorageFileNotFoundException(String.format("Could not read file: %s", filename));
+            }
+        }
+        catch (MalformedURLException mue)
+        {
+            throw new StorageFileNotFoundException(String.format("Could not read file: %s", filename), mue);
+        }
+    }
+
+    @Override
+    public void deleteAll()
+    {
+        FileSystemUtils.deleteRecursively(rootLocation.toFile());
+    }
+}
