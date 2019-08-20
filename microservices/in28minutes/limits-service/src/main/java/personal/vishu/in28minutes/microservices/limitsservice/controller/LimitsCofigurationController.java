e47ad1b099b68705a61ac59ad483e66b466f84e4
Vishwanath Kulkarni <Vishwanath.Kulkarni@microfocus.com>
2019-08-21 00:01:30 +0530

    adding hystrix, cloud bus and zipkin

--- microservices/in28minutes/limits-service/src/main/java/personal/vishu/in28minutes/microservices/limitsservice/controller/LimitsCofigurationController.java
+++ microservices/in28minutes/limits-service/src/main/java/personal/vishu/in28minutes/microservices/limitsservice/controller/LimitsCofigurationController.java
@@ -3,6 +3,9 @@ package personal.vishu.in28minutes.microservices.limitsservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import personal.vishu.in28minutes.microservices.limitsservice.bean.LimitConfigurationBean;
 import personal.vishu.in28minutes.microservices.limitsservice.configuration.LimitsConfiguration;
 
@@ -16,5 +19,17 @@ public class LimitsCofigurationController
     public LimitConfigurationBean retrieveLimitsFromConfiguration()
     {
         return new LimitConfigurationBean(configuration.getMaximum(), configuration.getMinimum());
+    }
+
+    @GetMapping("fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitConfigurationBean retrieveConfiguration()
+    {
+        throw new RuntimeException("Not Available");
+    }
+    
+    public LimitConfigurationBean fallbackRetrieveConfiguration()
+    {
+        return new LimitConfigurationBean(999, 9);
     }
 }
